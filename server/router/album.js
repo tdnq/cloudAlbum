833f40db967402ec8cf35382fdf3a72dc1d96d5a
tdnq <2397105389@qq.com>
2019-12-15 21:51:55 +0800

    init

--- server/router/album.js
+++
@@ -1,8 +0,0 @@
-const router =require("koa-router")();
-const album= require("../controller/album.js");
-router
-    .get("/",album.getAlbum)
-    .post("/",album.addAlbum)
-    .put("/",album.updateAlbum)
-    .del("/",album.deleteAlbum);
-module.exports = router.routes();
--- server/router/home.js
+++
@@ -1,5 +0,0 @@
-const router=require("koa-router")();
-const home =require("../controller/home.js");
-router
-    .get("/",home);
-module.exports=router.routes();
